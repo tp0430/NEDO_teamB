ae69d8b741c0e6c72bab27ab1a72eb5b034866da
tp0430 <56010304+tp0430@users.noreply.github.com>
2021-09-22 01:14:59 +0900

    first commitment

---
+++ hit_and_blow/display.py
@@ -0,0 +1,13 @@
+#coding: UTF-8
+"""
+    * File name: display.py
+    * Description: streamlitを使ったGUI用のモジュール
+    * Created on: September 22
+    * Created by: KENTA Mizuhara
+"""
+
+import streamlit as st
+
+def display_test():
+    # Streamlit が対応している任意のオブジェクトを可視化する (ここでは文字列)
+    st.write('Hello, World!')
