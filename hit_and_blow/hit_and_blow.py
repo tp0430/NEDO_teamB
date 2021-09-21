59cfb2b2a92bf25981e731bcc00c88b514aa5a7d
tp0430 <56010304+tp0430@users.noreply.github.com>
2021-09-22 01:15:39 +0900

    add description and change to use display module

--- hit_and_blow/hit_and_blow.py
+++ hit_and_blow/hit_and_blow.py
@@ -1,5 +1,15 @@
+#coding: UTF-8
+"""
+    * File name: hit_and_blow.py
+    * Description: hit and blowのmainモジュール
+    * Created on: September 22
+    * Created by: KENTA Mizuhara
+"""
+
+from display import display_test
+
 def main():
-    print("hello")
+    display_test()
 
 if __name__ == "__main__":
     main()
