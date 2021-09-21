9ab19b6cb3080986d97070a305376c2bfe978373
tp0430 <56010304+tp0430@users.noreply.github.com>
2021-09-22 01:16:09 +0900

    add description

--- tests/test_hit_and_blow.py
+++ tests/test_hit_and_blow.py
@@ -1,3 +1,11 @@
+#coding: UTF-8
+"""
+    * File name: test_hit_and_blow.py
+    * Description: hit_and_blow.pyのmain関数をテストする
+    * Created on: September 22
+    * Created by: KENTA Mizuhara
+"""
+
 from hit_and_blow.hit_and_blow import main
 
 def test_main():
