16a3d1a05285bc3cf360a60a5d7396764f704d7c
Takeaki Yamamoto <takeaki.yamamoto@gmail.com>
2021-09-29 13:56:58 +0900

    hit_and_blow-test

--- hit_and_blow/hit_and_blow.py
+++ hit_and_blow/hit_and_blow.py
@@ -6,13 +6,47 @@
     * Created by: KENTA Mizuhara
 """
 
-from display import display_test
+import requests
+import time
+import pprint
+
 from communication import APICom
 
+room_id = 2125
+player_name = "B"
+player_id = "7d025351-7836-4904-a48f-f58019b6ca77"
+hidden_num = "abcde"
 
-def main():
-    display_test()
+def input_init_info():
 
+    room_id = int(input("enter room id --- "))
+    player_name = input("enter player name --- ")
+    player_id = input("enter player id --- ")
+
+    hidden_num = input("enter hidden number --- ")
 
 if __name__ == "__main__":
-    main()
+
+    api_com = APICom(player_id=player_id, player_name=player_name, room_id=room_id)
+    result_enter_room = api_com.enter_room()
+
+    pprint(result_enter_room)
+
+    while api_com.get_room()["state"] == 1:
+        time.sleep(1)
+    
+    while api_com.get_room()["state"] == 2:
+
+        if api_com.get_table()["now_player"] == api_com.player_name:
+            guess_num = input("enter guess number --- ")
+
+            api_com.post_guess(guess_number = guess_num)
+        
+        time.sleep(1)
+    
+    if api_com.get_table()["winner"] == api_com.player_name:
+
+        print("YOU WIN !!")
+    else:
+        print("YOU LOSE")
+
--- tests/test_hit_and_blow.py
+++ tests/test_hit_and_blow.py
@@ -6,5 +6,47 @@
     * Created by: KENTA Mizuhara
 """
 
-def test_main():
-    assert 1 == 1
+import requests
+import time
+
+from hit_and_blow.communication import APICom
+
+room_id = 0
+player_name = ""
+player_id = ""
+room_id = 0
+hidden_num = "abcde"
+
+def input_init_info():
+
+    room_id = int(input("enter room id --- "))
+    player_name = input("enter player name --- ")
+    player_id = input("enter player id --- ")
+    room_id = input("enter room id --- ")
+
+    hidden_num = input("enter hidden number --- ")
+
+if __name__ == "__main__":
+
+    input_init_info()
+    api_com = APICom(player_id=player_id, player_name=player_name, room_id=room_id)
+    api_com.enter_room()
+
+    while api_com.get_room()["state"] == 1:
+        time.sleep(1)
+    
+    while api_com.get_room()["state"] == 2:
+
+        if api_com.get_table()["now_player"] == api_com.player_name:
+            guess_num = input("enter guess number --- ")
+
+            api_com.post_guess(guess_number = guess_num)
+        
+        time.sleep(1)
+    
+    if api_com.get_table()["winner"] == api_com.player_name:
+
+        print("YOU WIN !!")
+    else:
+        print("YOU LOSE")
+
