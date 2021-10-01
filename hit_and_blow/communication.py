d57f91ef169e4db99abfcfd10a37a03cf36839a7
Takeaki Yamamoto <takeaki.yamamoto@gmail.com>
2021-10-01 17:22:51 +0900

    17-update-communication

--- hit_and_blow/communication.py
+++ hit_and_blow/communication.py
@@ -19,22 +19,33 @@ class APICom:
     :param int room_id: ルームID
     """
 
-    def __init__(self, player_id, player_name, room_id):
-        self.URL = "https://damp-earth-70561.herokuapp.com"
-        self.session = requests.Session()
-        self.player_id = player_id
-        self.HEADERS = {"Content-Type": "application/json"}
-        self.player_name = player_name
+    def __init__(self, player_name, room_id):
 
-        self.room_id = room_id  # ここの値は2000~2999の範囲(使ってもいい部分)で都度変える。
+        self._URL = "https://damp-earth-70561.herokuapp.com"
+        self._session = requests.Session()
+        self._HEADERS = {"Content-Type": "application/json"}
+
+        self._player_list = self._register_player()
+
+        self._player_name = player_name
+        self._player_id = self._player_list[player_name]
+        self._room_id = room_id  # ここの値は2000~2999の範囲(使ってもいい部分)で都度変える。
+
+    def _register_player():
+        ret = {}
+        ret["B"] = "7d025351-7836-4904-a48f-f58019b6ca77"
+        ret["B2"] = "a9c2784a-2279-4215-bc7d-1255dbdf911d"
+
+        return ret
+
 
     def get_rooms(self):
         """全ての対戦部屋の情報を取得する.
         :return:
         """
-        url_get_all_rooms = self.URL + "/rooms/"
+        url_get_all_rooms = self._URL + "/rooms/"
 
-        result = self.session.get(url_get_all_rooms)
+        result = self._session.get(url_get_all_rooms)
 
         # print(result.status_code)
         return result.json()
@@ -46,11 +57,11 @@ class APICom:
 
         :return:
         """
-        url_enter_room = self.URL + "/rooms/"
-        enter_room_json = {"player_id": self.player_id, "room_id": self.room_id}
+        url_enter_room = self._URL + "/rooms/"
+        enter_room_json = {"player_id": self._player_id, "room_id": self._room_id}
 
-        result = self.session.post(
-            url_enter_room, headers=self.HEADERS, json=enter_room_json
+        result = self._session.post(
+            url_enter_room, headers=self._HEADERS, json=enter_room_json
         )
 
         # print(result.status_code)
@@ -60,9 +71,9 @@ class APICom:
         """指定した対戦部屋の情報を取得する
         :return:
         """
-        url_get_room = self.URL + "/rooms/" + str(self.room_id)
+        url_get_room = self._URL + "/rooms/" + str(self._room_id)
 
-        result = self.session.get(url_get_room)
+        result = self._session.get(url_get_room)
 
         # print(result.status_code)
         return result.json()
@@ -71,9 +82,9 @@ class APICom:
         """対戦情報テーブル(現在のターン, hit&blowの履歴, 勝敗の判定)を取得する.
         :return:
         """
-        url_get_table = self.URL + "/rooms/" + str(self.room_id) + "/players/" + self.player_name + "/table"
+        url_get_table = self._URL + "/rooms/" + str(self._room_id) + "/players/" + self._player_name + "/table"
 
-        result = self.session.get(url_get_table)
+        result = self._session.get(url_get_table)
 
         # print(result.status_code)
         return result.json()
@@ -83,10 +94,10 @@ class APICom:
         """相手が当てる5桁の16進数を登録する. ※アルファベットは小文字のみ
         :return:
         """
-        url_post_hidden = self.URL + "/rooms/" + str(self.room_id) + "/players/" + self.player_name + "/hidden"
-        post_hidden_json = {"player_id": self.player_id, "hidden_number": hidden_number}
+        url_post_hidden = self._URL + "/rooms/" + str(self._room_id) + "/players/" + self._player_name + "/hidden"
+        post_hidden_json = {"player_id": self._player_id, "hidden_number": hidden_number}
 
-        result = self.session.post(url_post_hidden, headers=self.HEADERS, json=post_hidden_json)
+        result = self._session.post(url_post_hidden, headers=self._HEADERS, json=post_hidden_json)
 
         # print(result.status_code)
         return result.json()
@@ -95,10 +106,22 @@ class APICom:
         """推測した数字を登録する
         :return:
         """
-        url_post_guess = self.URL + "/rooms/" + str(self.room_id) + "/players/" + self.player_name + "/table/guesses"
-        post_guess_json = {"player_id": self.player_id, "guess": guess_number}
+        url_post_guess = self._URL + "/rooms/" + str(self._room_id) + "/players/" + self._player_name + "/table/guesses"
+        post_guess_json = {"player_id": self._player_id, "guess": guess_number}
 
-        result = self.session.post(url_post_guess, headers=self.HEADERS, json=post_guess_json)
+        result = self._session.post(url_post_guess, headers=self._HEADERS, json=post_guess_json)
 
         # print(result.status_code)
         return result.json()
+
+    def get_game_state(self) -> int:
+        """ゲームの進行状態を取得
+        :return int :0: 取得失敗 1:待機中, 2:対戦中, 3:大戦終了
+        """
+        url_get_room = self._URL + "/rooms/" + str(self._room_id)
+        result = self._session.get(url_get_room)
+
+        if result.status_code == 200:
+            return result.json()["state"]
+        else:
+            return 0
