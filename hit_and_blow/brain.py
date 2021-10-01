5b61c60eb4c1dd7f23a33c44ac435986e9d8c732
Takeaki Yamamoto <takeaki.yamamoto@gmail.com>
2021-10-01 16:20:34 +0900

    unity to str in brain.py

--- hit_and_blow/brain.py
+++ hit_and_blow/brain.py
@@ -1,69 +1,51 @@
 import random
 import time
-from typing import Tuple, List
+from typing import Sized, Tuple, List
 
 ANS_LEN = 5
 MIN_ANS = 0
 MAX_ANS = 15
+MAX_ANSER_NUM = 524160
 
 class Brain:
 
     def __init__(self) -> None:
-        self.guess_num_list = self.make_all_number_list()
+        self.possible_answers = self.make_all_number_list()
         self.guess_history = []
         self.guess_result_history = []
         self.cnt = 0
 
-    def make_all_number_list(self) -> List[Tuple[int, int, int, int, int]]:
+    def make_all_number_list(self) -> List[str]:
         
         """全ての候補を取得
-        :return List[Tuple[int, int, int, int, int]]
+        :return List[str]
         """
         ret = []
 
-        for i in range(16):
-            for j in range(16):
+        for i in range(0x10):
+            for j in range(0x10):
                 if j == i:
                     continue
-                for k in range(16):
+                for k in range(0x10):
                     if k == j or k == i:
                         continue
-                    for l in range(16):
+                    for l in range(0x10):
                         if l == k or l == j or l == i:
                             continue
-                        for m in range(16):
+                        for m in range(0x10):
                             if m == l or m == k or m == j or m == i:
                                 continue
-                            ret.append((i, j, k, l, m))
+                            ret.append(hex(i)[2:] + hex(j)[2:] + hex(k)[2:] + hex(l)[2:] + hex(m)[2:])
+
         return ret
 
-    def select_guess_num_random(self):
-        return random.choice(self.guess_num_list)
+    def select_guess_num(self):
+        return random.choice(self.possible_answers)
 
-    def str2tuple(self, str_input: str) -> Tuple[int, int, int, int, int]:
-        """str型の入力をtupleに変換する。
-        :param str str_input: 16進数の入力
-        :return Tuple[int, int, int, int, int]: 10進数の出力
-        """
-
-        return tuple(int(i, 16) for i in str_input)
-    
-    def tuple2str(self, tuple_input: Tuple[int, int, int, int, int]) -> str:
-        """tuple型の入力をstrに変換する。
-        :param Tuple[int, int, int, int, int] tuple_input: 10進数の入力
-        :return str: 16進数の出力 
-        """
-
-        return_str = ""
-        for i in tuple_input:
-            return_str += str(hex(i)[-1])
-
-        return return_str
-    
-    def hit_and_blow(self, guess: Tuple[int, int, int, int, int], ans: Tuple[int, int, int, int, int]) -> Tuple[int, int]:
+    def hit_and_blow(self, guess: str, ans: str) -> Tuple[int, int]:
         """hit&blowのゲーム。
-        :param Tuple[int, int, int, int, int] guess: 予想した値。
-        :param Tuple[int, int, int, int, int] ans: 正解の値(本来は相手のものは分からない)。
+        :param str guess: 予想した値。
+        :param str ans: 正解の値(本来は相手のものは分からない)。
         :return Tuple[int, int]: hitとblowの値。
         """
 
@@ -76,14 +58,12 @@ class Brain:
 
         return (hit, blow)
 
-    def narrow_guess_num_list(self,
-        guess_num : Tuple[int, int, int, int, int] = None, 
-        guess_result : Tuple[int, int] = None):
+    def narrow_guess_num_list(self, guess_num : str = None, guess_result : Tuple[int, int] = None) -> int:
 
         """推測結果から候補の数を減らす。
-        :param Tuple[int, int, int, int, int] guess_num: 推測した数字
+        :param str guess_num: 推測した数字
         :param Tuple[int, int] guess_result: 推測した結果(hit, blow)
-        :return str: 減らした後の候補数
+        :return int: 減らした後の候補数
         """
 
         if guess_num == None:
@@ -91,15 +71,16 @@ class Brain:
         if guess_result == None:
             guess_result = self.guess_result_history[-1]
 
-        #元のリストから一つずつ要素を削除するとO(n^2)の計算時間がかかるので、違うリストを用意して移す
-        new_guess_num_list = []
-        for item in self.guess_num_list:
+        # 元のリストから一つずつ要素を削除するとO(n^2)の計算時間がかかるので、違うリストを用意して移す
+        new_possible_answers = []
+
+        for item in self.possible_answers:
             if guess_result == self.hit_and_blow(guess= guess_num, ans= item):
-                new_guess_num_list.append(item)
+                new_possible_answers.append(item)
 
-        self.guess_num_list = new_guess_num_list
+        self.possible_answers = new_possible_answers
 
-        return len(self.guess_num_list)
+        return len(self.possible_answers)
 
     def guess(self, guess_num_prev : str = None, guess_result_prev : Tuple[int, int] = None) -> str:
 
@@ -112,12 +93,12 @@ class Brain:
         self.cnt += 1
 
         if guess_num_prev != None:
-            self.guess_history.append(self.str2tuple(guess_num_prev))
+            self.guess_history.append(guess_num_prev)
             self.guess_result_history.append(guess_result_prev)
 
             self.narrow_guess_num_list()
 
-        return self.tuple2str(self.select_guess_num_random())
+        return self.select_guess_num()
 
 def make_number_random() -> str:
     """ランダムな重複しない5つの数字を作る。
@@ -168,16 +149,18 @@ if __name__ == "__main__":
         guess_result = None
         while 1:
             
+
             guess_num = brain.guess(guess_num, guess_result)
             guess_result = hit_and_blow(guess_num, ans)
             # print("{} : {}".format(guess_num, guess_result))
+            # print(len(brain.possible_answers))
 
             if guess_result[0] == 5:
 
-                if brain.cnt in times_to_correct:
-                    times_to_correct[brain.cnt] += 1
-                else:
-                    times_to_correct[brain.cnt] = 1
+                # if brain.cnt in times_to_correct:
+                #     times_to_correct[brain.cnt] += 1
+                # else:
+                #     times_to_correct[brain.cnt] = 1
                 # print("game finish!, {} times".format(brain.cnt))
                 break
     
