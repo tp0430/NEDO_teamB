e72b1f6fe67bca9d81766edf45bdac8ca9b30da2
ArowanaArowana <t.koike@mfg.t.u-tokyo.ac.jp>
2021-10-04 22:58:21 +0900

    use st.radio

--- hit_and_blow/hit_and_blow.py
+++ hit_and_blow/hit_and_blow.py
@@ -14,10 +14,10 @@ def main():
     st.title("Hit and Blow")
     player_name = st.text_input("PLAYER NAME")
     room_id = int(st.number_input("ROOM ID"))
-    player_type = st.text_input("PLAYER TYPE (manual/auto)")
-    if player_type == "manual":
+    player_mode = st.radio("Select game mode",["manual", "auto"])
+    if player_mode == "manual":
         game_player = Player(room_id=room_id, player_name=player_name)
-    elif player_type == "auto":
+    elif player_mode == "auto":
         game_player = Player(room_id=room_id, player_name=player_name, mode= 1)
     game_player.play_game()
 
