cda2214c0a7e33e4f8e21e7094fafc5395834fc0
Takeaki Yamamoto <takeaki.yamamoto@gmail.com>
2021-10-13 23:46:04 +0900

    fix 100 times

--- hit_and_blow/hit_and_blow_auto.py
+++ hit_and_blow/hit_and_blow_auto.py
@@ -2,20 +2,19 @@
 import sys
 
 from player import Player
+from auto_guess import AutoGuess
 
-ANS_LEN: int = 5
-MIN_ANS: int = 0
-MAX_ANS: int = 15
-REPEAT_NUM: int = 5
-PLAYER_NAME: str = "B2"
+REPEAT_NUM: int = 100
+PLAYER_NAME: str = "B"
 FIRST_ROOM_ID: int = 1  # 上野さん、対戦時に設定してください。
 
 
 def main(first_room_id = FIRST_ROOM_ID, repeat = REPEAT_NUM):
     for i in range(repeat):
-        player = Player(room_id= first_room_id + i,mode= 1, player_name= PLAYER_NAME)
+        player = Player(room_id= first_room_id + i, mode= 1, player_name= PLAYER_NAME)
+        player.auto_guesser = AutoGuess(strength= 10)
         player.play_game_internal()
-
+        print(i)
 if __name__ == "__main__":
 
     main(first_room_id= int(sys.argv[1]))
